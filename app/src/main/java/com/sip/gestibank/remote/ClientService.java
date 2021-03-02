a2b25975efcb378191a5747fe18c756f91715e7c
amine <amine.mezghich@ensi-uma.tn>
2021-03-02 18:37:20 +0100

    list des clients

---
+++ app/src/main/java/com/sip/gestibank/remote/ClientService.java
@@ -0,0 +1,34 @@
+package com.sip.gestibank.remote;
+
+
+import com.sip.gestibank.models.Client;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+public interface ClientService {
+
+    @GET("list/")
+    Call<List<Client>> getClients();
+
+    @GET("list/attente")
+    Call<List<Client>> getClientsEnAttentes();
+
+    @GET("list/valide")
+    Call<List<Client>> getClientsValides();
+
+    @POST("add/")
+    Call<Client> addClient(@Body Client client);
+/*
+    @PUT("update/{id}")
+    Call<User> updateUser(@Path("id") int id, @Body User user);
+
+    @DELETE("delete/{id}")
+    Call<User> deleteUser(@Path("id") int id);*/
+}
